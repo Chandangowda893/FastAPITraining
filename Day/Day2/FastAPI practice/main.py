77df6e907427fbf1772bd2c656d5ba65b7cbd847
Chandangowda893 <2025cs_chandan_b@nie.ac.in>
2026-09-22 13:21:09 +0530

    #path parameter

--- Day/Day2/FastAPI practice/main.py
+++ Day/Day2/FastAPI practice/main.py
@@ -9,3 +9,13 @@ def about():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+#post request
+@app.post("/create")
+def create_something():
+    return {"message": "created"}
+
+#path parameter
+@app.get("/student/{usn}")
+def get_student(usn):
+    return {"Result":"Distinction","usn":usn}
